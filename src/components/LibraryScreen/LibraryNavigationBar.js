6ec504b389df0b71e57f4512f733525a84e54e52
Xing Yi <blandilyte@gmail.com>
2023-10-20 15:31:59 +0800

    fix: button text colour in light theme

--- src/components/LibraryScreen/LibraryNavigationBar.js
+++ src/components/LibraryScreen/LibraryNavigationBar.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Appbar, Button } from 'react-native-paper';
+import { Appbar, Button, useTheme } from 'react-native-paper';
 import { localisedStrings } from '../../translations/l10n';
 
 export default function LibraryNavigationBar(props) {
+	const textColour = useTheme().colors.onSurface;
 	return (
 		<Appbar>
 			<View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
 				<Button
+					textColor={textColour}
 					onPress={() => props.navigation.navigate('Dictionaries')}
 				>
 					{localisedStrings['group-manager-card-title-dictionaries']}
 				</Button>
 				<Button
+					textColor={textColour}
 					onPress={() => props.navigation.navigate('Groups')}
 				>
 					{localisedStrings['library-screen-groups']}
 				</Button>
 				<Button
+					textColor={textColour}
 					onPress={() => props.navigation.navigate('Sources')}
 				>
 					{localisedStrings['library-screen-sources']}
