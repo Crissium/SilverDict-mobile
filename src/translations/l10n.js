47ea29a9237f9d9a9690b83b79e584f41ee11fdd
Yi Xing <blandilyte@gmail.com>
2024-02-05 23:29:36 +0800

    feat: new partial translation Russian

--- src/translations/l10n.js
+++ src/translations/l10n.js
@@ -4,7 +4,8 @@ import { RTL_LANGS } from '../config';
 export const localisedStrings = new LocalizedStrings({
 	'en-GB': require('./en-GB.json'),
 	'en-US': require('./en-US.json'),
-	'zh-CN': require('./zh-CN.json'),
+	'ru-RU': require('./ru-RU.json'),
+	'zh-CN': require('./zh-CN.json')
 });
 
 export const interfaceLangIsRTL = RTL_LANGS.includes(localisedStrings.getLanguage());
